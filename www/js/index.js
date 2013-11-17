13fc161c523e38bdb2e59ab1fd51ecaf93376453
ojread <oliver@oliverread.co.uk>
2013-11-17 21:45:39 +0000

    restarting

--- www/js/index.js
+++ www/js/index.js
@@ -1,45 +1,77 @@
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+	// Application Constructor
+	initialize: function() {
+		this.bindEvents();
+	},
+	// Bind Event Listeners
+	//
+	// Bind any events that are required on startup. Common events are:
+	// 'load', 'deviceready', 'offline', and 'online'.
+	bindEvents: function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
+	// deviceready Event Handler
+	//
+	// The scope of 'this' is the event. In order to call the 'receivedEvent'
+	// function, we must explicity call 'app.receivedEvent(...);'
+	onDeviceReady: function() {
+		app.receivedEvent('deviceready');
 
-        navigator.accelerometer.getCurrentAcceleration(
-            app.accelerometerSuccess,
-            app.accelerometerError
-        );
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+		accelerometer.startWatching();
+	},
+	// Update DOM on a Received Event
+	receivedEvent: function(id) {
+		var parentElement = document.getElementById(id);
+		var listeningElement = parentElement.querySelector('.listening');
+		var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+		listeningElement.setAttribute('style', 'display:none;');
+		receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    },
-    accelerometerSuccess: function(acceleration) {
-        alert('Acceleration X: ' + acceleration.x + '\n' +
-              'Acceleration Y: ' + acceleration.y + '\n' +
-              'Acceleration Z: ' + acceleration.z + '\n' +
-              'Timestamp: '      + acceleration.timestamp + '\n');
-    },
-    accelerometerError: function() {
-        alert("accelerometerError!");
-    }
+		console.log('Received Event: ' + id);
+	},
+	accelerometerSuccess: function(acceleration) {
+		alert('Acceleration X: ' + acceleration.x + '\n' +
+			  'Acceleration Y: ' + acceleration.y + '\n' +
+			  'Acceleration Z: ' + acceleration.z + '\n' +
+			  'Timestamp: '      + acceleration.timestamp + '\n');
+	},
+	accelerometerError: function() {
+		alert("accelerometerError!");
+	}
 };
+
+
+
+var accelerometer = {
+	watchID: null,
+
+	startWatching: function() {
+		var options = {
+			frequency: 1000
+		};
+
+		this.watchID = navigator.accelerometer.watchAcceleration(
+			this.success,
+			this.error,
+			options
+		);
+	},
+
+	success: function(acceleration) {
+		document.getElementById("x").innerHTML = acceleration.x;
+		document.getElementById("y").innerHTML = acceleration.y;
+		document.getElementById("z").innerHTML = acceleration.z;
+	},
+
+	error: function() {
+		document.getElementById("x").innerHTML = "Error";
+	},
+
+	stopWatching: function() {
+		if (watchID) {
+        	navigator.accelerometer.clearWatch(watchID);
+        	watchID = null;
+		}
+	}
+}
